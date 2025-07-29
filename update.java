cdc5e65097a79608fe80545b09e5ccf4d903d7c4
Max Rydahl Andersen <max@xam.dk>
2025-07-29 11:24:46 +0200

    update to handle quarkus blog is more verbose these days

--- update.java
+++ update.java
@@ -1,23 +1,31 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 //REPOS mavencentral,jitpack
-//DEPS io.quarkus:quarkus-bom:${quarkus.version:2.2.0.CR1}@pom
+//DEPS io.quarkus:quarkus-bom:${quarkus.version:3.25.0}@pom
 //DEPS io.quarkus:quarkus-qute
-//DEPS https://github.com/w3stling/rssreader/tree/v2.5.0
-//DEPS io.quarkus:quarkus-rest-client-reactive
-//DEPS io.quarkus:quarkus-rest-client-reactive-jackson
+//DEPS com.apptasticsoftware:rssreader:3.9.3
+//DEPS io.github.furstenheim:copy_down:1.1
+//DEPS io.github.markdown-asciidoc:markdown-to-asciidoc:2.0.1
 
-//JAVA 16+
 
-import com.apptastic.rssreader.Item;
-import com.apptastic.rssreader.RssReader;
+//DEPS io.quarkus:quarkus-rest-client
+//DEPS io.quarkus:quarkus-rest-client-jackson
+
+//JAVA 21+
+
+import com.apptasticsoftware.rssreader.Item;
+import com.apptasticsoftware.rssreader.RssReader;
+
+import io.github.furstenheim.CopyDown;
 import io.quarkus.qute.Engine;
 import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
-import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.QueryParam;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.QueryParam;
+import nl.jworks.markdown_to_asciidoc.Converter;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collection;
@@ -52,13 +60,16 @@ public class update implements QuarkusApplication {
                 """;
 
 
-        Collection<Item> sorted = new PriorityQueue<>(Collections.reverseOrder());
+        Collection<Item> sorted = new PriorityQueue<>();
         RssReader reader = new RssReader();
         Stream<Item> rssFeed = reader.read("https://xam.dk/blog/feed.atom");
         sorted.addAll(rssFeed.limit(3).collect(Collectors.toList()));
 
-        sorted.addAll(reader.read("https://quarkus.io/feed.xml").filter(p->p.getAuthor().get().contains("/maxandersen")).limit(3).collect(Collectors.toList()));
-
+        sorted.addAll(reader.read("https://quarkus.io/feed.xml")
+                        .filter(p->p.getAuthor().get().contains("/maxandersen")).limit(3)
+                        .map(p->{return extracted(p);})
+                        .collect(Collectors.toList()));
+ 
         Files.writeString(Path.of("readme.adoc"), qute.parse(Files.readString(Path.of("template.adoc.qute")))
                 .data("bio", bio)
                 .data("posts", sorted)
@@ -66,6 +77,13 @@ public class update implements QuarkusApplication {
                 .render());
 
         return 0;
+    }
+    private Item extracted(Item p) {
+        p.setDescription(Converter.convertMarkdownToAsciiDoc(new CopyDown().convert(p.getDescription().get())));
+        
+        p.setDescription(p.getDescription().get().substring(0, p.getDescription().get().indexOf("\n")));
+        
+        return p;
     }
 
  static public class Items{
@@ -75,7 +93,7 @@ public class update implements QuarkusApplication {
     public static interface PlaylistService {
 
         @GET
-        @javax.ws.rs.Path("/youtube/v3/playlistItems")
+        @jakarta.ws.rs.Path("/youtube/v3/playlistItems")
         List<Items> playlist(@QueryParam("part") String part,
                           @QueryParam("maxResults") int maxResults,
                           @QueryParam("playlistId") String playListId,
